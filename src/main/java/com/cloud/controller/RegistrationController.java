877c92d5b0d880f590abe794301200ee54c4f489
Max <kirkpatrickm@fontbonne.edu>
2018-04-30 13:41:26 -0500

    Fixed HTML to be compliant with thymeleaf by removing <hr> elements and adding th notation. Now attempting to fix why local host not rendering layout.html

--- src/main/java/com/cloud/controller/RegistrationController.java
+++ src/main/java/com/cloud/controller/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.cloud.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RegistrationController {
