2b91b9b4b9a707bcbf89303322eb92f0fb186879
Max <kirkpatrickm@fontbonne.edu>
2018-04-30 16:57:43 -0500

    Added controllers and java classes to register emails that are entered. Debugged thymeleaf and added "@Component"

--- src/main/java/com/cloud/controller/RegistrationController.java
+++ src/main/java/com/cloud/controller/RegistrationController.java
@@ -1,13 +1,33 @@
 package com.cloud.controller;
+import com.cloud.model.Attendee;
+import com.cloud.repository.AttendeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.stream.Collectors;
 
 @Controller
 public class RegistrationController {
 
+    @Autowired
+    AttendeeRepository attendeeRepository;
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("attendee", new Attendee());
         return "index";
     }
+
+    @RequestMapping(value="/register", method=RequestMethod.POST)
+    @ResponseBody
+    public String register(@ModelAttribute Attendee attendee){
+        attendeeRepository.addAttendee(attendee);
+        return attendeeRepository.allAttendeesAsString();
+    }
+
 }
